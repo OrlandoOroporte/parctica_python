8d6d13e84671d8abfe971fea67d0eb6d1b82c78d
OrlandoOroporte <orlando.oroporte@gmail.com>
2022-08-17 00:07:07 +0000

    contenido de clases 16-08-2022

--- list.py
+++ list.py
@@ -5,3 +5,10 @@ my_tupla = (1, 4, 3, 3, 5)
 my_set = {1, 4, 3, 3, 5}
 my_list_vacia = []
 print(my_list)
+
+#para cambiar la lista, un dato a la vez 
+my_list_vacia.append("orlando")
+my_list.insert(2, 756)
+
+print(my_list_vacia)
+print(my_list)
